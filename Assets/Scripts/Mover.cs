70bc80b17d154152d773b7f600c4e412e5796644
ChangwonDeveloper <changwondeveloper@gmail.com>
2023-04-12 20:34:41 +0900

    Introduction to Raycasting

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target;
+
+    NavMeshAgent navMeshAgent;
+
+    Ray lastRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
+        navMeshAgent.SetDestination(target.position);
+    }
+}
