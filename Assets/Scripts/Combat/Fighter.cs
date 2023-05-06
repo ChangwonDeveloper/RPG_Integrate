3a11a8cc35a5a42aebb4e3bf66d38d49e1fc56a1
ChangwonDeveloper <changwondeveloper@gmail.com>
2023-05-07 04:12:51 +0900

    Raycasting for components

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void Attack(CombatTarget target)
+        {
+            print("Take that you damage");
+        }
+    }
+}
--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         Mover mover;
+        Fighter fighter;
 
         // UNITY METHODS
 
         private void Awake()
         {
             mover = GetComponent<Mover>();
+            fighter = GetComponent<Fighter>();
         }
+
         private void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    fighter.Attack(target);
+                }
+                
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -25,13 +53,18 @@ namespace RPG.Control
         // PRIVATE METHODS
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = GetMouseRay();
             RaycastHit hit;
             bool hasHit = Physics.Raycast(ray, out hit);
             if (hasHit)
             {
                 mover.MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
